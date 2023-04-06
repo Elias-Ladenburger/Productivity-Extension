164c49ee1c81d531179e34ff6fc685b8108c2705
Elias Ladenburger <elias@ladenburger-family.de>
2023-04-06 15:59:48 +0200

    Build basic structure

---
+++ content-scripts/productivity.js
@@ -0,0 +1,10 @@
+
+/*
+Just draw a border round the document.body.
+*/
+document.body.style.border = "2px solid red";
+// Simulate an HTTP redirect:
+//window.location.replace("http://www.w3schools.com");
+
+// Simulate a mouse click:
+//window.location.href = "http://www.w3schools.com";
---
+++ popup/prod_booster.js
@@ -0,0 +1,20 @@
+  /**
+   * When the popup loads, inject a content script into the active tab,
+   * and add a click handler.
+   * If we couldn't inject the script, handle the error.
+   */
+  browser.tabs
+    .executeScript({ file: "/src/productivity.js" })
+    .then(listenForClicks)
+    .catch(reportExecuteScriptError);
+
+/**
+ * There was an error executing the script.
+ * Display the popup's error message, and hide the normal UI.
+ */
+function reportExecuteScriptError(error) {
+    document.querySelector("#popup-content").classList.add("hidden");
+    document.querySelector("#error-content").classList.remove("hidden");
+    console.error(`Failed to execute beastify content script: ${error.message}`);
+  }
+  
